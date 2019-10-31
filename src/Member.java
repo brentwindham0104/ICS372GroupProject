605ad0342011833e3a6a6312fb9aa80e3786abd4
Brent Windham <pz9580mc@my.century.edu>
2019-10-31 18:00:21 -0500

    Added method logic for member and memberList

--- src/Member.java
+++ src/Member.java
@@ -1,5 +1,10 @@
 import java.util.GregorianCalendar;
 
+/**
+ * 
+ * @author brentwindham
+ *This is the implementation of a member in the grocery store system.
+ */
 public class Member {
 	
 	private String memberId;
@@ -8,57 +13,132 @@ public class Member {
 	private double feePaid;
 	private String address;
 	private GregorianCalendar joinDate;
+	private int counter = 1;
 	
+	/**
+	 * Constructor for member.
+	 * @param name The name of the member.
+	 * @param address The address of the member.
+	 * @param phoneNumber The phone number of the member.
+	 * @param feePaid The fee paid by the member.
+	 */
 	public Member(String name, String address, int phoneNumber, double feePaid) {
 		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.feePaid = feePaid;
+		this.joinDate = new GregorianCalendar();
+		this.memberId = "m" + counter;
+		counter++;
 	}
 
+	/**
+	 * Getter for MemberId
+	 * @return memberId
+	 */
 	public String getMemberId() {
 		return memberId;
 	}
 
-	public void setMemberId(String memberId) {
+	/**
+	 * Setter for memberId
+	 * @param memberId The memberId of the member.
+	 * @return Returns true if completed.
+	 */
+	public boolean setMemberId(String memberId) {
 		this.memberId = memberId;
+		return true;
 	}
 
+	/**
+	 * Getter for name
+	 * @return Returns the name of the member
+	 */
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
+	/**
+	 * Setter for name
+	 * @param name The name of the member
+	 * @return Returns true if set
+	 */
+	public boolean setName(String name) {
 		this.name = name;
+		return true;
 	}
 
+	/**
+	 * Getter for phone number
+	 * @return Returns the members phone number
+	 */
 	public int getPhoneNumber() {
 		return phoneNumber;
 	}
 
-	public void setPhoneNumber(int phoneNumber) {
+	/**
+	 * Setter for phone number
+	 * @param phoneNumber The new phone number
+	 * @return Returns true if set
+	 */
+	public boolean setPhoneNumber(int phoneNumber) {
 		this.phoneNumber = phoneNumber;
+		return true;
 	}
 
+	/**
+	 * Getter for fee paid
+	 * @return Returns the fee paid
+	 */
 	public double getFeePaid() {
 		return feePaid;
 	}
-
-	public void setFeePaid(double feePaid) {
+	
+	/**
+	 * Setter for fee paid
+	 * @param feePaid The fee paid
+	 * @return Returns true if set
+	 */
+	public boolean setFeePaid(double feePaid) {
 		this.feePaid = feePaid;
+		return true;
 	}
-
+	
+	/**
+	 * Getter for address
+	 * @return Returns the address
+	 */
 	public String getAddress() {
 		return address;
 	}
 
-	public void setAddress(String address) {
+	/**
+	 * Setter for address
+	 * @param address The new address
+	 * @return Returns true if set
+	 */
+	public boolean setAddress(String address) {
 		this.address = address;
+		return true;
 	}
 
+	/**
+	 * getter for the join date
+	 * @return Returns the join date
+	 */
 	public GregorianCalendar getJoinDate() {
 		return joinDate;
 	}
 
-	public void setJoinDate(GregorianCalendar joinDate) {
+	/**
+	 * Setter for the join date
+	 * @param joinDate The new join date
+	 * @return Returns true if set
+	 */
+	public boolean setJoinDate(GregorianCalendar joinDate) {
 		this.joinDate = joinDate;
+		return true;
 	}
 	
 	
--- src/MemberList.java
+++ src/MemberList.java
@@ -1,31 +1,85 @@
+import java.util.Iterator;
 import java.util.LinkedList;
 
+/**
+ * 
+ * @author brentwindham
+ * This implements the collection of members.
+ */
 public class MemberList {
 	private LinkedList<Member> members = new LinkedList<Member>();
 	private static MemberList memberList;
 	
+	/**
+	 * Supports the singleton pattern
+	 * 
+	 * @return the singleton object
+	 */
 	public static MemberList getInstance() {
 		//TODO
-	}
+		if (memberList == null) {
+			return (memberList = new MemberList());
+		} else {
+			return memberList;
+		}	}
 	
+	/**
+	 * Private constructor for the memberlist object
+	 */
 	private MemberList() {
 		//TODO
+		
 	}
 	
+	/**
+	 * This adds a member to the list
+	 * @param member The member being added.
+	 * @return Returns true if added.
+	 */
 	private boolean addMember(Member member) {
 		//TODO
+		if (members.add(member)) {
+			return true;
+		}
+		return false;
 	}
 	
+	/**
+	 * This removes a member from the list.
+	 * @param memberId The id of the member being removed
+	 * @return Returns true if removed
+	 */
 	private boolean removeMember(String memberId) {
 		//TODO
+		if (members.remove(findMember(memberId))) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
+	/**
+	 * This finds a member in the list
+	 * @param memberId The id of the member that is being looked for.
+	 * @return Returns the member being looked for.
+	 */
 	private Member findMember(String memberId) {
 		//TODO
+		for (Member member : members) {
+			if (member.getMemberId().equals(memberId)) {
+				return member;
+			}
+		}
+		return null;
 	}
 	
+	/**
+	 * This supplies an iterator object of the list.
+	 * @return Returns an iterator object for the memberlist.
+	 */
 	private Iterator getMembers() {
 		//TODO
+		return members.iterator();
 	}
 }
 
