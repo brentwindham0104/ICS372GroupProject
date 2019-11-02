f54bc1affb8ef42e5d64628beefb842192fa162e
Isse Nur <issenur@gmail.com>
2019-11-02 12:11:45 -0500

    Fixed a few errors with the GroceryStoreImplTester.java

--- src/GroceryStoreImplTester.java
+++ src/GroceryStoreImplTester.java
@@ -8,11 +8,11 @@ public class GorceryStoreImplTester {
 
 	public static void main(String[] args) {
 		/*Creating a groceryStore Object for assert testing purposes*/
-		GroceryStore groceryStore = GroceryStore.getInstance();
+		GroceryStore.getInstance();
 		
 		
 		//Assert to check if members can be succesfully enrolled
-		assert (groceryStore.enrollMember("n1", 612, 10.0));
+		assert (GroceryStore.enrollMember("n1", 612, 10.0));
 		
 		/*This field holds the id for the first member object
 		in memberlist collection for test purposes*/
@@ -20,14 +20,14 @@ public class GorceryStoreImplTester {
 		
 
 		//Assert to check if members can be succesfully removed
-		assert (groceryStore.removeMember(memberId));
+		assert (GroceryStore.removeMember(memberId));
 		
 		/*Assert to check if removeMember works properly by returning
 		false when an unknown member removal is attempted*/
-		assert (!(groceryStore.removeMember("FakeId")));
+		assert (!(GroceryStore.removeMember("FakeId")));
 		
 		//Assert to check if a product can succesfully be added
-		assert (groceryStore.addProduct("p1",3,9.99, 100));
+		assert (GroceryStore.addProduct("p1",3,9.99, 100));
 			
 		/*This field holds the productId for the first product  object
 		in inventroy for test purposes*/
@@ -36,28 +36,28 @@ public class GorceryStoreImplTester {
 		
 		/*Assert to check if the addition of a non-new product will be
 		caught*/
-		assert (!(groceryStore.addProduct("p1",3,9.99, 100)));
+		assert (!(GroceryStore.addProduct("p1",3,9.99, 100)));
 		
 		//Assert to check if a shipment is successfully processed
-		assert (groceryStore.processShipment(productId, 199));
+		assert (GroceryStore.processShipment(productId, 199));
 		
 		/*Assert to check if processShipment would return false if
 		an unkown product is attempted to be sent as a shipment*/
-		assert (!(groceryStore.processShipment("FakeId", 199)));
+		assert (!(GroceryStore.processShipment("FakeId", 199)));
 		
 		//Assert to check if a product price is successfully changed
-		assert (groceryStore.changePrice(productId, 7.45));
+		assert (GroceryStore.changePrice(productId, 7.45));
 		
 
 		/*Assert to check if a changePrice method returns false
 		when a negative product price is entered*/
-		assert (!(groceryStore.changePrice(productId, -7.45)));
+		assert (!(GroceryStore.changePrice(productId, -7.45)));
 		
 		
 		//Assert to check if the Grocery Store object  is successfully serilized
-		assert (groceryStore.save();
+		assert (GroceryStore.save());
 		
 		//Assert to check if the Grocery Store object  is successfully de-serilized
-		assert (groceryStore.retrieve;
+		assert (GroceryStore.retrieve);
 	}
 }
