a2a85bff8147a884e938b51835964bde2057e0fa
Brent Windham <pz9580mc@my.century.edu>
2019-11-02 18:58:43 -0500

    Comments added to transaction classes

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,25 +1,86 @@
-import java.util.GregorianCalendar;
-import java.util.LinkedList;
-
-public class Transaction {
+import java.io.Serializable;
+import java.util.*;
+/**
+ * 
+ * @author brentwindham
+ * This implements the transaction of our grocery store system
+ *
+ */
+public class Transaction implements Serializable {
 	private GregorianCalendar saleDate;
 	private double total;
 	private Member member;
-	private LinkedList<LineItem> LineItemList = new LinkedList<LineItem>();
-	
+	private LinkedList<LineItem> lineItemList = new LinkedList<LineItem>();
+
+	/**
+	 * Constructor for transaction
+	 * @param member The member that is buying products
+	 */
 	public Transaction(Member member) {
-		//TODO
+		this.member = member;
+		this.saleDate = new GregorianCalendar(TimeZone.getTimeZone("Canada/Central"));
 	}
-	
+
+	/**
+	 * This adds a line item to the list
+	 * @param lineItem The lineItem being added
+	 * @return Returns true if added.
+	 */
 	public boolean addLineItem(LineItem lineItem) {
-		//TODO
+		return lineItemList.add(lineItem);
 	}
-	
+
+	/**
+	 * This is the iterator for the line items
+	 * @return Returns an iterator object
+	 */
+	public Iterator getLineItems(){
+		return lineItemList.iterator();
+	}
+
+	/**
+	 * This calculates the total price for all the line items
+	 * @return Returns the total price
+	 */
 	public double calculateTotal() {
-		//TODO
+		double runningTotal = 0.0;
+
+		for (LineItem lineItem: lineItemList) {
+			runningTotal += lineItem.getTotalPrice();
+		}
+
+		return runningTotal;
 	}
-	
+
+	/**
+	 * This gets the member of the transaction
+	 * @return Returns the member object
+	 */
 	public Member getMember() {
-		//TODO
+		return member;
+	}
+
+	/**
+	 * Getter for SaleDate
+	 * @return Returns SaleDate
+	 */
+	public GregorianCalendar getSaleDate() {
+		return saleDate;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder(String.format("%-30s%-30s%-30s%s", "Product Name", "Quantity", "Unit Price","total\n"));
+		for(LineItem lineItem : lineItemList){
+			stringBuilder.append(String.format("%-30s%-30s%-30s%s",
+					lineItem.getProduct().getName(),
+					lineItem.getQuantity(),
+					lineItem.getProduct().getPrice(),
+					lineItem.getTotalPrice()+"\n"));
+		}
+
+		stringBuilder.append(String.format("%-30s%s","Total",calculateTotal()+"\n"));
+		stringBuilder.append(String.format("%-30s%s","Date",saleDate.getTime()+"\n"));
+		return stringBuilder.toString();
 	}
 }
--- src/TransactionList.java
+++ src/TransactionList.java
@@ -1,22 +1,74 @@
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Iterator;
 import java.util.LinkedList;
-
-public class TransactionList {
+/**
+ * 
+ * @author brentwindham
+ *	This is the collection class for transactions
+ */
+public class TransactionList implements Serializable {
 	private LinkedList<Transaction> transactions = new LinkedList<Transaction>();
 	private static TransactionList transactionList;
-	
+
+	/**
+	 * Constructor for TransactionList
+	 */
 	private TransactionList() {
-		//TODO
 	}
-	
+
+	/**
+	 * Retrieves instance of TransactionList
+	 * @return Returns the TransactionList
+	 */
 	public static TransactionList getInstance() {
-		//TODO
+		if(transactionList == null) {
+			transactionList = new TransactionList();
+		}
+
+		return transactionList;
 	}
-	
+
+	/**
+	 * This adds a transaction to the list
+	 * @param transaction Transaction being added
+	 * @return Returns true if it is added
+	 */
 	public boolean addTransaction(Transaction transaction) {
-		//TODO
+		return transactions.add(transaction);
 	}
 	
-	public Iterator getTransactions(GregorianCalendar startDate, GregorianCalendar endDate) {
-		//TODO
+	/**
+	 * Retrieves the transactions that occured during a given time period
+	 * @param memberId Member that is a part of the transaction
+	 * @param startDate Beginning of the time period
+	 * @param endDate End of the time period
+	 * @return Returns the iterator object containing the transactions
+	 */
+	public Iterator getTransactions(String memberId, Calendar startDate, Calendar endDate) {
+		LinkedList<Transaction> transactionRange = new LinkedList<>();
+
+		for (Transaction transaction: transactions) {
+			if (!transaction.getMember().getMemberId().equals(memberId)) {
+				break;
+			}
+
+			Calendar date = transaction.getSaleDate();
+			if (startDate.get(Calendar.YEAR) > date.get(Calendar.YEAR)
+					&& startDate.get(Calendar.MONTH) > date.get(Calendar.MONTH)
+					&& startDate.get(Calendar.DATE) > date.get(Calendar.DATE)) {
+				break;
+			}
+
+			if (endDate.get(Calendar.YEAR) < date.get(Calendar.YEAR)
+					&& endDate.get(Calendar.MONTH) < date.get(Calendar.MONTH)
+					&& endDate.get(Calendar.DATE) < date.get(Calendar.DATE)) {
+				break;
+			}
+
+			transactionRange.add(transaction);
+		}
+
+		return transactionRange.iterator();
 	}
 }
